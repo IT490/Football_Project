0256d2e2a391f5596bebe22cb85dd391e836351f
Yuval <yk92@njit.edu>
2016-10-08 18:20:14 -0400

    Rewrote the autoloader,commented out modal, and added icons

--- index.php
+++ index.php
@@ -12,17 +12,6 @@
         integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
         crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script>
-      $(window).on('load', function() {
-        var succ = "<?php echo $_SESSION['success']; ?>";  
-        if( succ == "Yes" ){
-          $('#alert').replaceWith("<div class='alert alert-success' role='alert'><p><strong>Success! </strong>You are logged in.</p></div>");
-        }
-        if( succ == "No" ) {
-          $('#alert').replaceWith("<div class='alert alert-danger' role='alert'><h5><strong>No account! </strong>Please <a class='alert-link' href='index.php?controller=signupController'>Sign up</a></h5></div>");
-        }
-      });
-    </script>
     <!-- Still need to add Angular.min.js files -->
   </head>
   <body>
@@ -30,21 +19,17 @@
       <div class="container-fluid">
         <div class="navbar-header">
           <a class="navbar-brand" href="index.php"><img src="/img/footballIcon.jpg" alt="Logo" style="height:125%; width:125%;"></a>
-        </div>
-        <ul class="nav navbar-nav">
-          <li role="presentation" class="active"><a href="index.php">Home</a></li>
-      <!--    <li><a href="index.php?controller=LoginController">Mock Draft</a></li> -->
         </ul>
+        </div>
         <ul class="nav navbar-nav navbar-right">
           <li role="presentation">
-            <a href="#modal1" role="button" class="btn" data-toggle="modal"
-            ><span><i class="fa fa-sign-in"></i></span>&nbspLog In</a>
+            <a href="index.php?controller=loginController"><span><i class="fa fa-sign-in"></i></span>&nbspLog In</a>
           </li>
           <li role="presentation">
-            <a href="index.php?controller=DraftOptions">Mock Draft</a>
+            <a href="index.php?controller=DraftOptions"><span><i class="fa fa-cogs"></i></span>&nbspMock Draft</a>
           </li>
           <li role="presentation">
-            <a href="index.php?controller=PlayerList">Players</a>
+            <a href="index.php?controller=PlayerList"><i class="fa fa-users"></i></span>&nbspPlayers List</a>
           </li>
           <li role="presentation">
             <a href="index.php?controller=StartSit">Start/Sit</a>
@@ -52,9 +37,7 @@
         </ul>
       </div>
     </nav>
-    <div id="alert">
-    </div> 
-    <!-- Modal for Login starts here -->
+    <!-- Modal for Login starts here 
     <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="modal-1-label">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -63,7 +46,6 @@
             <h4 class="modal-title" id="modal-1-label"><strong>Please Log In</strong></h4>
           </div>
           <div class="modal-body modal-font">
-          <!-- form for log in goes here -->
           <form class="form-horizontal" action="rpc/testRpcClient.php" method='post'>
             <div class="form-group">
               <label for="username" class="col-sm-2 control-label">Username</label>
@@ -100,21 +82,26 @@
         </div>
       </div>
     </div>
+    -->
     <?php 
       function my_autoloader($class) {
-        include 'classes/' . $class . '.class.php';
+        if(strpos($class, 'Controller') !== false){
+          include 'classes/Controllers/' . $class . '.class.php';
+        }
+        else if(strpos($class, 'View') !== false) {
+          include 'classes/Views/' . $class . '.class.php';
+        }
+        else { include 'classes/' . $class . '.class.php'; }
       }
       spl_autoload_register('my_autoloader');
 
-      //db connection here through rabbitMQ
-    
       $app = new app;
     ?>
     
     <nav class="navbar navbar-default navbar-fixed-bottom">
       <div class="container">
         <p class="navbar-text pull-right">
-          &copy; <?php echo date("Y"); ?> JBY Inc.
+          A Jb-Y Production &copy <?php echo date("Y"); ?> 
         </p>
       </div>
     </nav>
