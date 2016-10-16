ed486fafe77059f7ef79e8153641b9c6922a8070
br66 <br66@njit.edu>
2016-10-15 21:50:01 -0400

    with bootstrap and page header

--- classes/Views/playerListView.class.php
+++ classes/Views/playerListView.class.php
@@ -10,12 +10,42 @@ class playerListView extends View
 		$nfl = $client->nfl;
 
 		$collection = $client->nfl->Players;
-		
+	
+		$this->html .= '<div class="page-header" style="margin-top: 60px;">
+				<h2 class>&nbsp<span><i class="fa fa-lg fa-archive"></i></span>&nbspPlayer List</h2>
+				</div>';
+
+		$this->html .= '<br><div class="container">
+					<table class="table table-hover table-responsive">
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Team</th>
+								<th>College</th>
+								<th>Position</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>	
+				';
+	
 		foreach ($collection->find() as $document)
 		{
-			$this->html .= '<br><br>'. $document["FirstName"] . ' ' . $document["LastName"] .' <br>';
-
+			$this->html .= '<tr><td>'. 
+					$document["FirstName"]. ' ' . $document["LastName"].
+					'</td><td>'. 
+					$document["CurrentTeam"].
+					'</td><td>'.
+					$document["College"].
+					'</td><td>'.
+					$document["Position"].
+					'</td><td>'.
+					$document["CurrentStatus"]. 
+					'</td></tr>';
 		}
+
+		$this->html .= '</tbody></table></div>';
+				
 
 		return $this->html;
 	}
