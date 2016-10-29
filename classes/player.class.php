f78db6910198028ccc66d4d91555693dd94b6e9f
br66 <br66@njit.edu>
2016-10-29 19:31:25 -0400

    changed grid for player profile, working on curl request for news...

--- classes/player.class.php
+++ classes/player.class.php
@@ -30,20 +30,27 @@ class player
 			// build player profile
                 	$this->html .= '<div class="container">
 					<div class="row">
-						<div class="col-xs-8">
+						<div class="col-sm-2">
 							<div class="main-content">
 								<img src=' . "\"" . $array[0]["PhotoUrl"] . "\"" . '/>
 							</div>
-						<div class="col-xs-8">
-							<div class="main-content">'.
-								$array[0]["BirthDateString"].'<br>'.
-								$array[0]["Position"] .'<br>'.
-								$array[0]["CurrentTeam"].'<br>'.
-								$array[0]["College"].'<br>
+						</div>
+						<div class="col-sm-10">
+							<h3>'.$array[0]['Name'].'</h3>
+							<div class="row">
+								<div class="col-sm-12">'.
+									$array[0]['Age'].'<br>'.
+									$array[0]['BirthDateString'].'<br>'.
+									$array[0]['Position'].'<br>'.
+									$array[0]['CurrentTeam'].'<br>'.
+									$array[0]['College'].'<br>'.
+									$array[0]['Experience'].'<br>
+								</div>	
 							</div>
 						</div>
-				</div>';
+					</div>';
 
+					//$curl = curl_init();
 		}
 
 		return $this->html;
