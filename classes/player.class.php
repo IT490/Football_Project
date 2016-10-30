75bd1a021819c016b11c102e21f53a84543e8ef8
Yuval <yk92@njit.edu>
2016-10-30 00:55:10 -0400

    Integrated arrest info with player class

--- classes/player.class.php
+++ classes/player.class.php
@@ -23,7 +23,16 @@ class player
 
 		$documents = $collection->find( [ 'Name' =>  $this->name  ]);
 		$array = $documents->toArray();
-		$result_count = count($array);
+    $result_count = count($array);
+
+    $ch = curl_init();
+    $url = "http://www.nflarrest.com/api/v1/player/arrests/" . $this->name;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close();
+
+    $arrest_data = json_decode($output, true);
 
 		if ($result_count == 1)
 		{
@@ -50,7 +59,7 @@ class player
 							                </div>
 						               </div>
 					              </div>';
-
+    
 /*$ch = curl_init(); //initialize curl channel
  *$url = "WHATEVER THE URL IS FOR API" . Name or whatever //create url string
  *curl_setopt($ch, CURLOPT_URL, $url); //set the url
@@ -60,7 +69,27 @@ class player
  *$response = json_decode($output) //decode the json object it returns, this is not regular json if you vardump you will see that curl returns an object (StdClass Object) 
  */
     }
+    if ($arrest_data === NULL) 
+      $this->html .= '<div class="panel-default" style="margin-top: 40px; width: 1150px;">
+                        <h3>This player has never been arrested! Amazing...</h3>
+                      </div>';
+    else {
+      $this->html .= '<div class="panel-default" style="margin-top: 40px; width: 1150px;">
+                        <div class="panel-header">
+                          <h3 style="margin-left: 10px;">Arrest Information</h3>
+                        </div>
+                        <ul class="list-group">';
+      foreach($arrest_data as $record) {
+        $this->html .= '<li class="list-group-item">
+                          <h4><span><i class="fa fa-thumbs-down"></i></span><i class="text-danger">&nbspOn ' . $record["Date"] . " " . $this->name . ' was arrested for ' . $record["Category"] . '</i></h4>' . 
+                          '<strong>Description: </strong>' . $record["Description"] . '</br>' .
+                          '<strong>Outcome: </strong>' . $record["Outcome"] .
+                       '</li>';
+      }
 
-		return $this->html;
-	}
+      $this->html .= '</ul></div>';
+    }
+
+    return $this->html;
+  }
 }
