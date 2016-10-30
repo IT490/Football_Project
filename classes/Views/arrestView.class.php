28690873f96d4f2e9edae4cba85e589ddc526171
Yuval <yk92@njit.edu>
2016-10-30 17:06:28 -0400

    Updated arrestView

--- classes/Views/arrestView.class.php
+++ classes/Views/arrestView.class.php
@@ -19,16 +19,46 @@
                          }).responseText;
                          
                          var data = new google.visualization.arrayToDataTable(eval(jsonData));
-                         var options = {
-                            title: 'Top crimes in NFL'
+                         var piechart_options = {
+                            title: 'Top crimes in NFL',
+                            width: 400,
+                            height: 300
                          };
                          
-                         var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-                         chart.draw(data, options);
+                         var piechart = new google.visualization.PieChart(document.getElementById('piechart_div'));
+                         piechart.draw(data, piechart_options);
+
+                         var barchart_options = {
+                            title: 'Top 10 Crimes in NFL',
+                            width: 600,
+                            height: 300
+                         };
+                         var barchart = new google.visualization.BarChart(document.getElementById('barchart_div'));
+                         barchart.draw(data, barchart_options);
                        }
 
                      </script>
-                     <div id='chart_div'></div>";
+                     <table class='columns' style='margin-top: 60px;'>
+                       <tr>
+                         <td><div id='piechart_div' style='border: 1px solid #ccc'></div></td>
+                         <td><div id='barchart_div' style='border: 1px solid #ccc'></div></td>
+                       </tr>
+                     </table>
+                     <div class='panel-default' style='margin-top: 40px; width: 500px;'>
+                       <h4>Search by Player</h4>
+                       <form class='form-inline'>
+                         <div class='form-group'>
+                           <div class='col-sm-12 input-group'>
+                             <span class='input-group-addon><i class='fa fa-search'></i></span>
+                             <input type='text' class='form-control' id='search' name='search' placeholder='Search by Player name'>
+                           </div>
+                         </div>
+                         <div class='form-group'>
+                           <div class='col-sm-4'>
+                             <button type='submit' class='btn btn-success'>Search</button>
+                           </div>
+                         </div 
+                     </div>";
 
       return $this->html;
     }
