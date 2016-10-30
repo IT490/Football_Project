a243f94ad01de943f880d42fc0ce3f2c23316dcb
br66 <br66@njit.edu>
2016-10-30 01:16:38 -0400

    before master pull: cannot get results from curl request

--- classes/player.class.php
+++ classes/player.class.php
@@ -50,7 +50,23 @@ class player
 						</div>
 					</div>';
 
-					//$curl = curl_init();
+					$curl = curl_init();
+					$url = 'https://api.newsriver.io/v2/search?query=text%3A%C22'.
+                                                                        $array[0]['FirstName'].
+                                                                        '%20'.
+                                                                        $array[0]['LastName'].
+                                                                        '%22%20AND%20text%3Anfl&limit=10';
+					echo($url);
+					curl_setopt($curl, CURLOPT_URL, $url);
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+											'Authorization: sBBqsGXiYgF0Db5OV5tAw0wqH79z9TnUTVyUeDVG_EwMCvyLAbILcwAuIRd-9iR0'
+											));
+					$output = curl_exec($curl);
+					curl_close($curl);
+
+					$news = json_decode($output, true);
+					var_dump($news);
 		}
 
 		return $this->html;
