e66c185e62edd6e4693319d1b640208b8b7c25ae
Yuval <yk92@njit.edu>
2016-10-31 15:47:03 -0400

    Added player Model

--- index.php
+++ index.php
@@ -16,9 +16,6 @@
     <script src="js/bootstrap.min.js"></script>
     <!-- Still need to add Angular.min.js files -->
     <link rel="stylesheet" href="css/main.css">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js" charset="utf-8"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.js"></script>
-    <script type="text/javascript" src="https://rawgithub.com/NickQiZhu/dc.js/master/web/js/crossfilter.js"></script>
   </head>
   <body>
     <nav id="navbar" class="navbar navbar-default navbar-fixed-top">
@@ -48,52 +45,6 @@
       </div>
     </nav>
     <div class="main-div">
-    <!-- Modal for Login starts here 
-    <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="modal-1-label">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="modal-1-label"><strong>Please Log In</strong></h4>
-          </div>
-          <div class="modal-body modal-font">
-          <form class="form-horizontal" action="rpc/testRpcClient.php" method='post'>
-            <div class="form-group">
-              <label for="username" class="col-sm-2 control-label">Username</label>
-              <div class="col-sm-10">
-                <input type="email" class="form-control" id="username" name="user" placeholder="Username">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="password" class="col-sm-2 control-label">Password</label>
-              <div class="col-sm-10">
-                <input type="password" class="form-control" id="password" name="pw" placeholder="Password">
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="col-sm-offset-2 col-sm-10">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Remember Me
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Sign In</button>
-              </div>
-            </div>
-          </form>
-          </div>
-          <div class="modal-footer">
-            <h5>If you do not have an account...</br></h5>
-            <button type="button" class="btn btn-primary">Sign Up</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    -->
     <?php 
       function my_autoloader($class) {
         if(strpos($class, 'Controller') !== false){
@@ -101,6 +52,9 @@
         }
         else if(strpos($class, 'View') !== false) {
           include 'classes/Views/' . $class . '.class.php';
+        }
+        else if (strpos($class, 'Model') !== false) {
+          include 'classes/Models/' . $class . '.class.php';
         }
         else { include 'classes/' . $class . '.class.php'; }
       }
