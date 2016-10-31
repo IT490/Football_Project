7bf36d5c42767f38edc0c23b8c612605fe1dffc7
Yuval <yk92@njit.edu>
2016-10-30 21:55:46 -0400

    Styled arrestView better

--- classes/Views/arrestView.class.php
+++ classes/Views/arrestView.class.php
@@ -21,8 +21,8 @@
                          var data = new google.visualization.arrayToDataTable(eval(jsonData));
                          var piechart_options = {
                             title: 'Top crimes in NFL',
-                            width: 400,
-                            height: 300
+                            width: 500,
+                            height: 400
                          };
                          
                          var piechart = new google.visualization.PieChart(document.getElementById('piechart_div'));
@@ -30,35 +30,35 @@
 
                          var barchart_options = {
                             title: 'Top 10 Crimes in NFL',
-                            width: 600,
-                            height: 300
+                            width: 700,
+                            height: 400
                          };
                          var barchart = new google.visualization.BarChart(document.getElementById('barchart_div'));
                          barchart.draw(data, barchart_options);
                        }
 
                      </script>
-                     <table class='columns' style='margin-top: 60px;'>
+                     <div class='panel-default' style='margin-top: 40px; width: 500px;'>  
+                       <h4>Search by Player</h4>                                          
+                       <form class='form-inline'>                                         
+                         <div class='form-group'>                                         
+                           <div class='col-sm-12 input-group'>                            
+                             <span class='input-group-addon><i class='fa fa-search'></i></span>
+                             <input type='text' class='form-control' id='search' name='search' placeholder='Search by Player name'>
+                           </div>                                                         
+                         </div>                                                           
+                         <div class='form-group'>                                         
+                           <div class='col-sm-4'>                                         
+                             <button type='submit' class='btn btn-success'>Search</button>
+                           </div>                                                         
+                         </div                                                            
+                     </div>               
+                     <table class='columns' style='margin: auto; margin-top: 30px;'>
                        <tr>
                          <td><div id='piechart_div' style='border: 1px solid #ccc'></div></td>
                          <td><div id='barchart_div' style='border: 1px solid #ccc'></div></td>
                        </tr>
-                     </table>
-                     <div class='panel-default' style='margin-top: 40px; width: 500px;'>
-                       <h4>Search by Player</h4>
-                       <form class='form-inline'>
-                         <div class='form-group'>
-                           <div class='col-sm-12 input-group'>
-                             <span class='input-group-addon><i class='fa fa-search'></i></span>
-                             <input type='text' class='form-control' id='search' name='search' placeholder='Search by Player name'>
-                           </div>
-                         </div>
-                         <div class='form-group'>
-                           <div class='col-sm-4'>
-                             <button type='submit' class='btn btn-success'>Search</button>
-                           </div>
-                         </div 
-                     </div>";
+                     </table>";
 
       return $this->html;
     }
