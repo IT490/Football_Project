69019d3e4340b08136040ed60f97ac722a562a14
Yuval <yk92@njit.edu>
2016-12-15 14:03:37 -0500

    Added startSit view to master

--- classes/Views/startSitView.class.php
+++ classes/Views/startSitView.class.php
@@ -1,43 +1,38 @@
 <?php
 
-  class startSitView extends View {
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-    public function getHTML() {
+  class startSitView extends View 
+	{
+
+    public function getHTML() 
+		{
       //create the Start/Sit view
+
       $this->html .= "<div class='page-header' style='margin-top: 60px;'>
-                        <h2 class='text-left'>&nbsp<span><i class='fa fa-lg fa-archive'></i></span>&nbspSign in</h2>
+                        <h2 class='text-left'>&nbsp<span><i class='fa fa-lg fa-bed'></i></span>&nbspStart/Sit Tool</h2>
                       </div>
-                      <form class='form-horizontal' method='post' action='index.php?controller=loginController'>
-                        <div class='form-group'>
-                          <label for='username' class='col-sm-2 control-label'>Username: </label>
-                          <div class='col-sm-4 input-group'>
-                            <span class='input-group-addon'><i class='fa fa-hand-spock-o'></i></span>
-                            <input type='text' class='form-control' id='user' name='user' placeholder='Enter your username'>
-                          </div>
-                        </div>
-                        <div class='form-group'>
-                          <label for='password' class='col-sm-2 control-label'>Password: </label>
-                          <div class='col-sm-4 input-group'>
-                            <span class='input-group-addon'><i class='fa fa-key'></i></span>
-                            <input type='password' class='form-control' id='pw' name='pw' placeholder='Enter password'>
-                          </div>
-                        </div>
-                        <div class='form-group'>
-                          <div class='col-sm-offset-2 col-sm-4'>
-                            <div class='checkbox'>
-                              <label>
-                                <input type='checkbox' name='remember'>I'm Lazy
-                              </label>
-                            </div>
-                          </div>
-                        </div>
-                        <div class='form-group'>
-                          <div class='col-sm-offset-2 col-sm-4'>
-                            <button type='submit' class='btn btn-success'>Sign in</button>
-                          </div>
-                        </div>
-                      </form>";
-                              
+
+											<form class='form-inline' method='post' action='index.php?controller=startSitController'>
+												<div class='form-group'>
+													<div class='col-sm-12 input-group'>
+														<span class='input-group-addon><i class='fa fa-search'></i></span>
+														<input type='text' class='form-control' id='player1' name='player1' placeholder='Search by player name'>
+													</div>
+												</div>
+												<div class='form-group'>
+													<div class='col-sm-12 input-group'>
+														<span class='input-group-addon><i class='fa fa-search'></i></span>
+														<input type='text' class='form-control' id='player2' name='player2' placeholder='Search by player name'>
+													</div>
+												</div>
+												<div class='form-group'>
+													<div class='col-sm-4'>
+														<button type='submit' class='btn btn-success'>Search</button>
+													</div>
+												</div>
+											</form>
+                      ";        
       return $this->html;
     }
 
